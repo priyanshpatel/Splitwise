3db3df6da5078a210d4ddb08d591e38e686d557c
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-02-27 05:02:38 -0800

    Landing page layout

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,13 +5,14 @@ import {Route} from 'react-router-dom';
 // import Delete from './Delete/Delete';
 // import Create from './Create/Create';
 import Navbar from './LandingPage/Navbar';
+import LandingPage from './LandingPage/LandingPage';
 //Create a Main Component
 class Main extends Component {
     render(){
         return(
             <div>
                 {/*Render Different Component based on Route*/}
-                {<Route path="/" component={Navbar}/>}
+                {<Route path="/" component={LandingPage}/>}
                 {/* // <Route path="/login" component={Login}/>
                 // <Route path="/home" component={Home}/>
                 // <Route path="/delete" component={Delete}/>
